b83de29cd4ada7be8e16aa11d3c9095af3a1b806
Maxim <forfor@mail.ru>
2020-03-11 23:45:34 +0300

    first commite

---
+++ script.js
@@ -0,0 +1,38 @@
+'use strict';
+
+
+a=5;
+console.log(a);
+
+var number = 5;
+var string = "Hello";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+console.log(4/0);
+
+console.log('string'*9);
+let something;
+console.log(something);
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(persone["name"]);
+let arr = ['plum.png','orange.jpg','apple.bmp'];
+
+console.log(arr[1]);
+//alert("Hello World!");
+//let answer = confirm("Are ypu here&");
+//console.log(answer);
+//let answer = prompt("Есть ли вам 18 лет?", "Да");
+//console.log(typeof(answer));
+console.log("arr" + " - object");
+
+
